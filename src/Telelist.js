3fe50ac8af04e330533448e68813a650578232b9
marieme78 <aidaramarieme2@gmail.com>
2024-01-18 13:55:25 +0000

    update

--- src/Telelist.js
+++ src/Telelist.js
@@ -7,12 +7,15 @@ import "./App.css";
 import Card from "react-bootstrap/Card";
 import "bootstrap/dist/css/bootstrap.min.css";
 
+// ...
+
 const Telelist = ({ movies }) => {
   const [data, setData] = useState(movie);
   const handleaddmovie = (newmovie) => {
     const updatemovie = [...data, { ...newmovie }];
     setData(updatemovie);
   };
+  
   // État pour gérer la valeur de l'input de recherche
   const [searchInput, setSearchInput] = useState("");
 
@@ -23,17 +26,30 @@ const Telelist = ({ movies }) => {
 
   // Fonction à appeler lors du clic sur le bouton de recherche (search).
   const search = () => {
-    // Filtrer les films qui ont la même note que la valeur de l'input (insensible à la casse)
-    const matchingMovies = data.filter((movie) => movie.note == searchInput);
+    // Filtrer les films dont le genre contient la valeur de l'input (insensible à la casse)
+    const matchingMovies = data.filter(
+      (movie) => movie.note.toLowerCase().includes(searchInput.toLowerCase())
+    );
 
     if (matchingMovies.length > 0) {
       // Retourner les films correspondants
-      return matchingMovies.map((movie) => console.log(movie));
+      return matchingMovies.map((movie) => (
+        <div key={movie.id}>
+          <Moviecard {...movie} />
+        </div>
+      ));
     } else {
       // Aucun film trouvé, retourner un message ou un composant indiquant qu'aucun film n'a été trouvé
-      return console.log("Aucun film trouvée");
+      return (
+        <div>
+          <p>Aucun film trouvé.</p>
+        </div>
+      );
     }
   };
+
+  // ...
+
   return (
     <>
       <div
@@ -57,9 +73,15 @@ const Telelist = ({ movies }) => {
         </Button>{" "}
       </div>
       <div className="cardplayer">
-        {data.map((film, index) => (
-          <Moviecard key={index} {...film} />
-        ))}
+        {searchInput ? (
+          // Afficher les résultats de la recherche
+          search()
+        ) : (
+          // Afficher tous les films si aucun filtre n'est appliqué
+          data.map((film, index) => (
+            <Moviecard key={index} {...film} />
+          ))
+        )}
       </div>
       <Createfilm addMovie={handleaddmovie} />
     </>
